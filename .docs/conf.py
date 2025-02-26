5985789fb7b4c5b720366f33dd962f0e75432db5
Gregory R. Macchio <gm0796@princeton.edu>
2025-02-26 16:15:22 -0500

    Mock tqdm import on readthedocs

--- .docs/conf.py
+++ .docs/conf.py
@@ -49,4 +49,4 @@ html_theme_options = {'navigation_depth': 10}
 
 
 # mocking the import of torch because it is not installed on readthedocs
-autodoc_mock_imports = ["torch"]
+autodoc_mock_imports = ["torch", "tqdm"]
